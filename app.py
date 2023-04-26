9bbd0c7d3c60bb3037ee1a4cc74e86a91e033eca
Jaskaran Singh <67426155+jaskaran5109@users.noreply.github.com>
2023-04-26 23:28:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
-liver_model = pickle.load(open('liver_model.sav', 'rb'))
+liver_model = pickle.load(open('./liver_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
